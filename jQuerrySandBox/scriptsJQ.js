1b10a356385ca460c355f8b71f0628a7928dbfba
Kubanczyk85 <39081031+JakubSliwinski85pl@users.noreply.github.com>
2018-06-13 15:58:33 +0200

    day3 last commit

--- jQuerrySandBox/scriptsJQ.js
+++ jQuerrySandBox/scriptsJQ.js
@@ -1,4 +1,27 @@
-$('document').ready(function (){
-    var p = $('p');
-    console.log(p.length);
-})
+// $('document').ready(function (){
+//     var p = $('p');
+//     console.log(p.length);
+// })
+// tak wyglada pełna skłądnia załadowania skryptu na zaladownaie strony.
+
+//.on - na kliknięcie   .one- na klikniecie tylko raz
+
+
+$(function (){
+    var $body  = $('body')   //chashowanie selektora//
+
+    $body.on('click', 'p', addNewParagraph)
+
+    // function addNewParagraph (event){
+    //     console.log(event);
+    //     console.log(this);   //this - zawiera kontekst wywołani funkcji//
+    //     $body.append('<p>Nowy paragraf</p>')
+    //
+    //     var p = $('p');
+    //     console.log(p.length);
+    // }
+
+
+    function addNewParagraph (event){
+    $(event.target).after('<p>Nowy paragraf</p>');}  //
+});
