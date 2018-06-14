cbdd711057f71f862b4e6cd221692a518e91cb58
Kubanczyk85 <39081031+JakubSliwinski85pl@users.noreply.github.com>
2018-06-14 10:05:49 +0200

    day 4 commit 3

--- test_BoxClick/BoxClick_script.js
+++ test_BoxClick/BoxClick_script.js
@@ -5,16 +5,27 @@ $(function (){
     $body.on('click', '.box' ,  addMyProperty);
 
     function addMyProperty (event){
-        var $target = $(event.target);
-        var isClicked =  $target.hasClass('clicked');
-        var rowIsFull = $target.parent().children().length > 4 ;
+        var $target = $(event.target);                                  //zamieniam sobie czesto wyszukiwany kawalem na zmienną
+        var isClicked =  $target.hasClass('clicked');                   //zamieniam sobie czesto wyszukiwany property na zmienną
+        var $row = $target.closest('.row');                             //zamieniam sobie czesto wyszukiwany kawalem na zmienną
+        var rowIsFull = $row.find('.box:not(.template)').length > 4 ;   //znajdz najblizszy element od
+                                                                        // targeta idąc po DOM w górę taki
+                                                                        // jak podany w paramtrze 'closest(x)'
+                                                                        // i znajdz element taki jak w parametrze find(x.
 
         if(isClicked || rowIsFull){
-            console.log('nie dodaję');   }
-          else {$target
-            .after('<div class="box">nowy</div>')
-            .addClass('clicked');
-            console.log('dodaję');
+            console.log('nie dodaję');
+            }
+            else {
+
+                var $NewBox = $row.find('.box.template')
+                    .clone()
+                    .removeClass('template');
+
+                $target
+                .after($NewBox)
+                .addClass('clicked');
+                console.log('dodaję');
         }
     }
 });
