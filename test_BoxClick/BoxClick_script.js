66640b1bb8fa8ef3e2d55a7f06faef45837c3627
Kubanczyk85 <39081031+JakubSliwinski85pl@users.noreply.github.com>
2018-06-14 10:14:04 +0200

    day 4 commit next

--- test_BoxClick/BoxClick_script.js
+++ test_BoxClick/BoxClick_script.js
@@ -5,13 +5,14 @@ $(function (){
     $body.on('click', '.box' ,  addMyProperty);
 
     function addMyProperty (event){
-        var $target = $(event.target);                                  //zamieniam sobie czesto wyszukiwany kawalem na zmienną
-        var isClicked =  $target.hasClass('clicked');                   //zamieniam sobie czesto wyszukiwany property na zmienną
-        var $row = $target.closest('.row');                             //zamieniam sobie czesto wyszukiwany kawalem na zmienną
-        var rowIsFull = $row.find('.box:not(.template)').length > 4 ;   //znajdz najblizszy element od
+        var $target = $(event.target);                                  // <- zamieniam sobie czesto wyszukiwany kawalem na zmienną
+        var isClicked =  $target.hasClass('clicked');                   // <- zamieniam sobie czesto wyszukiwany property na zmienną
+        var $row = $target.closest('.row');                             // <- zamieniam sobie czesto wyszukiwany kawalem na zmienną
+        var boxesCount = $row.find('.box:not(.template)').length       // <- znajdz najblizszy element od
                                                                         // targeta idąc po DOM w górę taki
                                                                         // jak podany w paramtrze 'closest(x)'
-                                                                        // i znajdz element taki jak w parametrze find(x.
+                                                                        // i znajdz element taki jak w parametrze find(x)
+        var rowIsFull = boxesCount > 4 ;
 
         if(isClicked || rowIsFull){
             console.log('nie dodaję');
@@ -21,6 +22,8 @@ $(function (){
                 var $NewBox = $row.find('.box.template')
                     .clone()
                     .removeClass('template');
+
+                $NewBox.text($NewBox.text() + (boxesCount +1));
 
                 $target
                 .after($NewBox)
