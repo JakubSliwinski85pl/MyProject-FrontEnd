d8b85b9378bfc671b85115c1acb5770a595570fc
Kubanczyk85 <39081031+JakubSliwinski85pl@users.noreply.github.com>
2018-06-15 11:55:30 +0200

    day 5 przed obiadem

--- invoice-vanilla/invoice-calculation.js
+++ invoice-vanilla/invoice-calculation.js
@@ -1,36 +1,31 @@
 (function(){                                                                       //dodanie izolujacej funkcji , ktora na koncu jest
                                                                                    // wywoływana przez () n końcu
                                                                                    // zaraz po wywołaniu przez function(){}()
-
-document.addEventListener('DOMContentLoaded',init);                                 //czekamy na załadowanie się strony i całego DOM ,
-                                                                                    // bez jquerry musimy to obsłuzyć w js
-
-function init() {                                                                   //akcja ktorą wywołujemy już docelowo
+document.addEventListener('DOMContentLoaded',init);                                //czekamy na załadowanie się strony i całego DOM ,
+                                                                                   // bez jquerry musimy to obsłuzyć w js
+Łfunction init() {                                                                  //akcja ktorą wywołujemy już docelowo
     console.log('hello from vanilla');
-
             var tbody =   document.querySelector('tbody');
                 tbody.addEventListener('input',handleItemChange);
 
-   function handleItemChange (event){
+       function handleItemChange (event){
        if (event.target.type === 'number' || event.target.tagName ==='SELECT')      //ten if powoduje że działa tylko na polach przyjmujacych
                                                                                     // number i polach select , bez pól typu strng
         //   console.log('handleItemChange',event);
        setItemSum(event.target.closest('tr'));
        }
 
-   function setItemSum(tr){
+       function setItemSum(tr){
     //   console.log('setItemSum',event);
        var items = tr.querySelector('[name="txtFldItems"]').value;
        var net = tr.querySelector('[name="txtFldNetValue"]').value;
        var vat = tr.querySelector('[name="txtFldVAT"]').value;
 
-      console.log(items ,net, vat ,event);
+      //console.log(items ,net, vat ,event);
 
-      var sum = items*net*vat;                                                      //mnożenie danych
+      var sum = items * net * vat;                                                      //mnożenie danych
       tr.querySelector('[name="txtfldGrossValue"]').value = sum.toFixed(2);         //usupełnianie danych w polu 'brutto'
    }
-
-
 
 }
 })();
